226425d768b72687a047ea0e748aec939bb3c814
Chahid12a <160957412+Chahid12a@users.noreply.github.com>
2024-03-11 18:16:23 +0000

    owner-reporte.js

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -1,5 +1,5 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚁𝙴𝙿𝙾𝚁𝚃𝙴*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} el comando ${usedPrefix}play no manda nada*`;
+  if (!text) throw `*[❗معلومة❗] تحقيق في تقرير*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} el comando ${usedPrefix}play no manda nada*`;
   if (text.length < 10) throw `*[❗إنتباه!❗] !المرجو كتابة 10 كلمات على الاقل*`;
   if (text.length > 1000) throw `*[❗إنتباه!❗] يمكنك كتاببة اقل من 1000 كلمة!*`;
   const teks = `*❒═════[التقرير]═════❒*\n*┬*\n*├❧ 𝙽𝚄𝙼𝙴𝚁𝙾:* wa.me/${m.sender.split`@`[0]}\n*┴*\n*┬*\n*├❧ 𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${text}\n*┴*`;

d7a93c55353a4183d2805cb746ae58e602370fe6
Chahid12a <160957412+Chahid12a@users.noreply.github.com>
2024-03-12 11:22:36 +0000

    owner-reporte.js

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -2,8 +2,8 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (!text) throw `*[❗معلومة❗] تحقيق في تقرير*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} el comando ${usedPrefix}play no manda nada*`;
   if (text.length < 10) throw `*[❗إنتباه!❗] !المرجو كتابة 10 كلمات على الاقل*`;
   if (text.length > 1000) throw `*[❗إنتباه!❗] يمكنك كتاببة اقل من 1000 كلمة!*`;
-  const teks = `*❒═════[التقرير]═════❒*\n*┬*\n*├❧ 𝙽𝚄𝙼𝙴𝚁𝙾:* wa.me/${m.sender.split`@`[0]}\n*┴*\n*┬*\n*├❧ 𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${text}\n*┴*`;
-  conn.reply('212679556171@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  const teks = `*❒═════[التقرير]═════❒*\n*┬*\n*├❧ 𝙽𝚄𝙼𝙴𝚁𝙾:* wa.me/${m.sender.split`@`[0]}\n*┴*\n*┬*\n*├❧ رسالة* ${text}\n*┴*`;
+  conn.reply('https://wa.me/212679556171', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
   m.reply(`*[ ✔️ ] سيتم معالجة الخطأ الدي في البوت قريبا.. بعد مراسلة صاحب البوت*`);
 };
 handler.help = ['reporte', 'request'].map((v) => v + ' <teks>');

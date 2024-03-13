8290f14a56525ec1fe3ad1cecfc67e098895ed51
Chahid12a <160957412+Chahid12a@users.noreply.github.com>
2024-03-13 12:49:07 +0000

    Update audio-efectos.js

--- plugins/audio-efectos.js
+++ plugins/audio-efectos.js
@@ -32,7 +32,7 @@ const handler = async (m, {conn, args, __dirname, usedPrefix, command}) => {
           ptt: true,
         });
       });
-    } else throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰𝙻 𝙰𝚄𝙳𝙸𝙾 𝙾 𝙽𝙾𝚃𝙰 𝙳𝙴 𝚅𝙾𝚉 𝙴𝙻 𝙲𝚄𝙰𝙻 𝚂𝙴𝚁𝙰 𝙼𝙾𝙳𝙸𝙵𝙸𝙲𝙰𝙳𝙾, 𝚄𝚂𝙰𝙳𝙾 𝙴𝙻 𝙲𝙾𝙰𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`;
+    } else throw `*[❗ معلومات ❗] قم بالرد على الصوت أو ملاحظة الصوت التي سيتم تعديلها، باستخدام الأمر . ${usedPrefix + command}*`;
   } catch (e) {
     throw e;
   }

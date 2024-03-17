2c14c5e7e4dfba4c26b3733bf2eeab9338d3de02
Chahid12a <160957412+Chahid12a@users.noreply.github.com>
2024-03-17 14:34:26 +0000

    Update telegraph.js

--- plugins/telegraph.js
+++ plugins/telegraph.js
@@ -4,7 +4,7 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'هذا الامر خاص برفع الصور لموقع\nhttps://telegra.ph/\nالان قم بالاشارة للصورة التي تريد رفعها للموقع وسوف تحصل على رابطها \n اشر اليها ثم اكتب\n*.telegraph*'
+  if (!mime) throw 'هذا الامر خاص برفع الصور لموقع\nhttps://telegra.ph/\nالان قم بالاشارة للصورة التي تريد رفعها للموقع وسوف تحصل على رابطها \n اشر اليها ثم اكتب\n*.tr'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
@@ -16,5 +16,5 @@ let handler = async (m) => {
 }
 handler.help = ['tourl']
 handler.tags = ['uploader']
-handler.command = ['telegraph', 'tourl','dir']
+handler.command = ['telegraph', 'tr','dir']
 export default handler;
